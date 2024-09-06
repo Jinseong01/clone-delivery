e808426fff96e8c3e7cac7ed083e521ce386c732
jinsung <jinsung528@gmail.com>
2024-09-07 02:30:04 +0900

    filter

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 2) Json 변환 시의 snakecase 설정 등을 위해 custom한 ObjectMapperConfig 생성
 3) API 문서화 등 유용한 swagger ui 설정
 4) Swagger Ui에도 custom한 ObejctMapper를 사용하기 위해 SwaggerConfig 생성
-5) 
+5) filter를 통해 클라이언트에 응답하기 전, request와 response에 대한 로그
 
 - db 패키지
 1) Entity의 공통되는 id부분을 BaseEntity로 따로 구성
---
+++ api/src/main/java/org/delivery/api/filter/LoggerFilter.java
@@ -0,0 +1,60 @@
+package org.delivery.api.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class LoggerFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        //체인 실행 전
+        //리퀘스트와 리스폰스 내용을 여러번 읽을 수 있도록 하기 위한 wrapper 클래스
+        var req = new ContentCachingRequestWrapper((HttpServletRequest) servletRequest);
+        var res = new ContentCachingResponseWrapper((HttpServletResponse) servletResponse);
+
+
+        //체인 실행
+        filterChain.doFilter(req, res);
+
+        //체인 실행 후
+        //request 정보
+        var headerNames = req.getHeaderNames();
+        var headerValues = new StringBuilder();
+
+        headerNames.asIterator().forEachRemaining(headerKey -> {
+            var headerValue = req.getHeader(headerKey);
+            headerValues.append("[").append(headerKey).append(" : ").append(headerValue).append("] ");
+        });
+
+        var requestBody = new String(req.getContentAsByteArray());
+        var uri = req.getRequestURI();
+        var method = req.getMethod();
+
+        log.info(">>>> uri : {}, method : {}, header : {}, body : {}", uri, method, headerValues, requestBody);
+
+        //response 정보
+        var responseHeaderValues = new StringBuilder();
+
+        res.getHeaderNames().forEach(headerKey -> {
+            var headerValue = req.getHeader(headerKey);
+
+            responseHeaderValues.append("[").append(headerKey).append(" : ").append(headerValue).append("] ");
+        });
+
+        var responseBody = new String(res.getContentAsByteArray());
+
+        log.info("<<<< uri : {}, method : {}, header : {}, body : {}", uri, method, responseHeaderValues, responseBody);
+
+        //캐시된 res의 body부분 내용을 실제로 전송하는 res에 담기위 위한 메소드
+        res.copyBodyToResponse();
+    }
+}
